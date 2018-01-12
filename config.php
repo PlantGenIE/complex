d8f6472ae0e04376df787ce9bbdd21d15be57234
Niklas Mähler <niklas.mahler@umu.se>
2018-01-12 08:33:25 +0100

    Remove basket connection settings from config

--- config.php
+++ config.php
@@ -7,11 +7,6 @@ $config["db"]["user"] = null;
 $config["db"]["password"] = null;
 $config["db"]["database"] = null;
 
-$config["basket_db"]["host"] = null;
-$config["basket_db"]["user"] = null;
-$config["basket_db"]["password"] = null;
-$config["basket_db"]["database"] = null;
-
 require_once("local_config.php");
 
 ?>
