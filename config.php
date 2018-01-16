911686e3f7ba76030ced0667cd66950b80e740f6
Niklas Mähler <niklas.mahler@umu.se>
2018-01-16 14:04:32 +0100

    Add database port to config

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ $config["db"]["host"] = null;
 $config["db"]["user"] = null;
 $config["db"]["password"] = null;
 $config["db"]["database"] = null;
+$config["db"]["port"] = 3306;
 
 require_once("local_config.php");
 
