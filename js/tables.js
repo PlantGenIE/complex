8369f1a947802b18385a9351cff5bddf13e03a34
Niklas Mähler <niklas.mahler@umu.se>
2018-01-22 22:55:54 +0100

    Add checkbox to table rows

--- js/tables.js
+++ js/tables.js
@@ -2,11 +2,22 @@ var firsttable;
 var secondtable;
 
 var table_options = {
-    searching: false
+    searching: false,
+    columns: [
+        {
+            data: "selected",
+            render: function(data, type, row, meta) {
+                var checkbox = '<input type="checkbox" value="' +
+                    data.id + '" ' + (data.is_selected ? ' checked' : '') + '/>';
+                return type === 'display' ? checkbox : data;
+            }
+        },
+        {data: "gene"}
+    ]
 };
 
 function ComplexTable(element) {
-    this.datatable = $(element).dataTable(table_options);
+    this.datatable = $(element).DataTable(table_options);
     $(element + " thead th").each(function() {
         if ($(this).index() != 0) {
             var title = $(this).text();
@@ -15,7 +26,19 @@ function ComplexTable(element) {
     });
 
     this.set_data = function(data) {
-        return;
+        this.datatable.clear();
+        var table_data = $.grep(data, function(x) {
+            return x.group === 'nodes';
+        }).map(function(x) {
+            return {
+                selected: {
+                    is_selected: typeof(x.selected) == "undefined" ? false : x.selected,
+                    id: x.data.id
+                },
+                gene: x.data.label
+            };
+        });
+        this.datatable.rows.add(table_data).draw();
     };
 }
 
