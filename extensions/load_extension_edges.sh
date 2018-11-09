3e9413912139beb627309e5a840e15de89eedcdd
Niklas Mähler <niklas.mahler@umu.se>
2018-11-09 08:59:18 +0100

    More efficient loading of extension data

--- extensions/load_extension_edges.sh
+++ extensions/load_extension_edges.sh
@@ -22,7 +22,7 @@ if [[ ! -f "$1" ]]; then
     echo >&2 "error: file or directory not found: $1"
 fi
 
-genes="$1"
+input="$1"
 name="$2"
 database="$3"
 
@@ -30,22 +30,36 @@ read -p "MySQL user: " mysql_user
 read -sp "MySQL password: " mysql_pwd
 echo
 
-while read line; do
-    gene1=$(awk '{print $1}' <(echo $line))
-    network1=$(awk '{print $2}' <(echo $line))
-    gene2=$(awk '{print $3}' <(echo $line))
-    network2=$(awk '{print $4}' <(echo $line))
-    score=$(awk '{print $5}' <(echo $line))
-    query="INSERT IGNORE INTO edge_extension (gene_id1, gene_id2, network_id1, network_id2, score, extension_name) VALUES ((SELECT id FROM gene WHERE name = '$gene1'), (SELECT id FROM gene WHERE name = '$gene2'), $network1, $network2, $score, '$name');"
-    output=$(mysql \
-        --user=$mysql_user \
-        --password=$mysql_pwd \
-        --database=$database \
-        --execute="$query" 2>&1) || true
-    if grep --quiet 'ERROR' <(echo $output); then
-        echo >&2 $output
-        exit
-    fi
-    echo $query
-    grep -v 'Using a password on the command line' <(echo $output) || true
-done < $genes
+# Test password
+if ! mysql \
+    --user=$mysql_user \
+    --password=$mysql_pwd \
+    --database=$database \
+    --execute='quit' \
+    >/dev/null 2>&1; then
+    echo >&2 "error: failed to connect to MySQL with user $mysql_user"
+    exit 1
+fi
+
+echo "Generating query..."
+awkcommand='{
+    printf("INSERT INTO edge_extension (gene_id1, gene_id2, network_id1, network_id2, score, extension_name) VALUES ((SELECT id FROM gene WHERE name = \"%s\"), (SELECT id FROM gene WHERE name = \"%s\"), %d, %d, %f, \"%s\");\n", $1, $3, $2, $4, $5, ext);
+}'
+query=$(awk -v ext=$name "$awkcommand" $input)
+
+echo "Removing existing extension details..."
+delete_output=$(mysql \
+    --user=$mysql_user \
+    --password=$mysql_pwd \
+    --database=$database \
+    --execute="DELETE FROM edge_extension WHERE extension_name = \"$name\"" \
+    2>&1 || :)
+echo $delete_output | grep -v 'Using a password on the command line' || :
+
+echo "Executing query..."
+insert_output=$(mysql \
+    --user=$mysql_user \
+    --password=$mysql_pwd \
+    --database=$database \
+    < <(printf "$query") 2>&1 || :)
+echo $insert_output | grep -v "Using a password on the command line" || :
--- extensions/load_extension_nodes.sh
+++ extensions/load_extension_nodes.sh
@@ -20,7 +20,7 @@ if [[ ! -f "$1" ]]; then
     echo >&2 "error: file or directory not found: $1"
 fi
 
-genes="$1"
+input="$1"
 name="$2"
 database="$3"
 
@@ -28,18 +28,36 @@ read -p "MySQL user: " mysql_user
 read -sp "MySQL password: " mysql_pwd
 echo
 
-while read line; do
-    gene=$(awk '{print $1}' <(echo $line))
-    query="INSERT IGNORE INTO gene_extension (gene_id, extension_name) VALUES ((SELECT id FROM gene WHERE name = '$gene'), '$name');"
-    output=$(mysql \
-        --user=$mysql_user \
-        --password=$mysql_pwd \
-        --database=$database \
-        --execute="$query" 2>&1) || true
-    if grep --quiet 'ERROR' <(echo $output); then
-        echo >&2 $output
-        exit
-    fi
-    echo $query
-    grep -v 'Using a password on the command line' <(echo $output) || true
-done < $genes
+# Test password
+if ! mysql \
+    --user=$mysql_user \
+    --password=$mysql_pwd \
+    --database=$database \
+    --execute='quit' \
+    >/dev/null 2>&1; then
+    echo >&2 "error: failed to connect to MySQL with user $mysql_user"
+    exit 1
+fi
+
+echo "Generating query..."
+awkcommand='{
+    printf("INSERT INTO gene_extension (gene_id, extension_name) VALUES ((SELECT id FROM gene WHERE name = \"%s\"), \"%s\");\n", $1, table)
+}'
+query=$(awk -v table=$name "$awkcommand" $input)
+
+echo "Removing existing extension details..."
+delete_output=$(mysql \
+    --user=$mysql_user \
+    --password=$mysql_pwd \
+    --database=$database \
+    --execute="DELETE FROM gene_extension WHERE extension_name = \"${name}\"" \
+    2>&1 || :)
+echo $delete_output | grep -v "Using a password on the command line" || :
+
+echo "Executing query..."
+insert_output=$(mysql \
+    --user=$mysql_user \
+    --password=$mysql_pwd \
+    --database=$database \
+    < <(echo $query) 2>&1 || :)
+echo $insert_output | grep -v 'Using a password on the command line' || :
